81f154e7531612c752658c0b7edf02564c52aad4
Dave Eum <eumjungsik@gmail.com>
2020-01-14 18:27:28 -0500

    I finished level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,7 +2,7 @@
 var tableData = data;
 var button = d3.select("button");
 var tbody = d3.select("tbody");
-
+var inputDateElement = d3.select(".form-control");
 
 // YOUR CODE HERE!
 /*var data = [{
@@ -19,39 +19,51 @@ var tbody = d3.select("tbody");
 //     console.log(ufoReport);
 //   });
 
-
-  tableData.forEach((ufoReport) => {
+tableData.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-
-
-
-  button.on("click", function() {
-    console.log("Hi you pressed the button!")
-    // Select the input element and get the raw HTML node
-    var inputDateElement = d3.select(".form-control");
-  
-    // Get the value property of the input element
-    var inputDate = inputDateElement.property("value");
-    
-    console.log(inputDate);
-    
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputDate);
-
-    var changedData = filteredData;
-
-    changedData.forEach((queryReport) => {
-    var row = tbody.append("tr");
-    Object.entries(queryReport).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
     });
+});
+
+inputDateElement.on("change", function() {
+    var newText = d3.event.target.value;
+    //console.log(newText);
+});
+  
+
+
+
+  
+button.on("click", function() {
+    tbody.html("");
+    console.log("Hi you pressed the button!");
+    // Select the input element and get the raw HTML node    
+    // Get the value property of the input element
+    var inputDate = inputDateElement.property("value");
+    //console.log(inputDate);
+    function matchDate(x){
+
+    return x.datetime == inputDate;
+    };
+
+    var dateQuery = tableData.filter(matchDate);
     
-  });
+//   var table = document.getElementById ("ufo-table");
+//   console.log(table);
+//   var tr = d3.selectAll("tr");
+//   console.log(tr);
+//   //tr.remove();
+
+    dateQuery.forEach((i) => {
+        var row = tbody.append("tr");
+        Object.entries(i).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+});
 
     
     //var tableMain = d3.select(".summary");
@@ -59,5 +71,5 @@ var tbody = d3.select("tbody");
     //list.html("");
   
    
-  });
+  
   
